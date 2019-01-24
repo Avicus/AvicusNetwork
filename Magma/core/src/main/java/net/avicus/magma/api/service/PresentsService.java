44499aa4a8f7423b127b3eb74f706a86eb52e40b
Austin Mayes <almayes@trevecca.edu>
2019-01-24 17:03:24 -0600

    Fix POM version

--- Magma/core/src/main/java/net/avicus/magma/api/service/PresentsService.java
+++ Magma/core/src/main/java/net/avicus/magma/api/service/PresentsService.java
@@ -1,13 +1,13 @@
 package net.avicus.magma.api.service;
 
 import java.util.Optional;
-import javafx.util.Pair;
 import net.avicus.magma.api.APIClient;
 import net.avicus.magma.api.graph.MutationResponse;
 import net.avicus.magma.api.graph.QLBuilder;
 import net.avicus.magma.api.graph.inputs.PresentFindInput;
 import net.avicus.magma.api.graph.types.present.Present;
 import net.avicus.magma.database.model.impl.User;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class PresentsService extends Service<Present> {
 
@@ -19,7 +19,7 @@ public class PresentsService extends Service<Present> {
     Optional<MutationResponse> res = getClient().mutation(QLBuilder.mutation(
         m -> m.presentFind(new PresentFindInput(who.getId(), slug, family).setCreate(true),
             q -> q.success().message())));
-    return new Pair<>(res.map(p -> p.getData().getPresentFind().isSuccess()).orElse(false),
-        res.map(p -> p.getData().getPresentFind().getMessage()).orElse("Failed to contact API!"));
+    return Pair.of(res.map(p -> p.getData().getPresentFind().isSuccess()).orElse(false),
+                   res.map(p -> p.getData().getPresentFind().getMessage()).orElse("Failed to contact API!"));
   }
 }
