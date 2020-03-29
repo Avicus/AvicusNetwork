d62f5ec2a5a80d42568df32b16ff958a6b237ee4
Austin Mayes <austin@avicus.net>
2020-03-29 14:59:44 -0500

    Actually fix vote spam

--- Atlas/core/src/main/java/net/avicus/atlas/module/vote/VoteModule.java
+++ Atlas/core/src/main/java/net/avicus/atlas/module/vote/VoteModule.java
@@ -143,7 +143,7 @@ public final class VoteModule implements Module {
 
   void cast(final Player player, final Match vote) {
     if (this.votes.containsKey(player.getUniqueId()) &&
-        this.votes.get(player.getUniqueId()).getId().equals(match.getId())) return;
+        this.votes.get(player.getUniqueId()).getId().equals(vote.getId())) return;
 
     Events.call(new PlayerCastVoteEvent(player));
     this.votes.put(player.getUniqueId(), vote);
